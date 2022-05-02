62489c56149fb81678538568fd9101bc61d801e7
Ricky Dodd <rickydoddmobile@gmail.com>
2022-05-02 23:06:40 +1000

    Integrate RSS link in Footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 export default function Footer() {
   return (
     <footer>
         <p>Created using <a href="https://nodejs.org/">node.js</a>, <a href="https://reactjs.org/">React</a> and <a href="https://www.gatsbyjs.com/">Gatsby</a>.</p>
+        <p><Link to="/rss.xml">RSS Feed</Link></p>
     </footer>
   )
 }
